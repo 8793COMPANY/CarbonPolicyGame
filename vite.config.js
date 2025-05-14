7dedca01a03c9679bfa8b8f3a530e0080d3dea5e
KSY9808 <kimsy9808@naver.com>
2025-05-14 14:12:10 +0900

    수정 : main.jsx에서 BrowserRouter를 HashRouter로 변경

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CarbonPolicyGame/'  // ✅ GitHub 리포지토리 이름과 동일
 })
